68d3410404c2f2f5ab2d42f100cc9555311a35c7
Krishna93951 <kkagile123@gmail.com>
2019-10-31 10:15:35 +0530

    3 files modified

--- src/controller.js
+++ src/controller.js
@@ -1,54 +1,89 @@
 function init() {
-  controllerInstance().selectStorageType();
+  controllerInstance.selectStorageType();
   viewInstance().emptyList();
-  viewInstance().emptyListMsg();
-}
+};
 
-function createStorageInstance() {
-  var selectedOption = document.getElementById("storage").value;
-  var key = "taskListKK";
-  var storage = {
-    LocalStorage: "LocalStorage",
-    SessionStorage: "SessionStorage"
-  };
-  return new StorageManager(storage[selectedOption], key);
-}
+var controllerInstance = new App(prompt("Please Enter Storage Key"))
 
-function storeData(taskData) {
-  createStorageInstance().setData(taskData);
-}
+App.prototype.addTaskOnClick = function() {
+  controllerInstance.addTasks();
+};
 
-function getData() {
-    return createStorageInstance().getData();
-}
-
-function controllerInstance() {
-  return new App();
-}
-
-function App() {
+App.prototype.addTaskOnKeyPress = function(e) {
   var this_ = this;
+  if (e.keyCode === 13) {
+    controllerInstance.addTasks();
+  }
+};
+
+App.prototype.tasksCount = function() {
+  var list = document.querySelector("#todoList");
+  var taskData = this.getData();
+  var checkbox = list.querySelectorAll('input[type="checkbox"]:checked');
+  var pending;
+  var checkedCount = 0;
+  for (var i = 0; i < checkbox.length; i++) {
+    checkboxCount = checkbox[i].checked ? checkedCount++ : checkedCount--;
+  }
+  pending = taskData.length - checkedCount;
+  viewInstance().totalMsg(checkedCount, pending);
+};
+
+App.prototype.clearAndFocusTextField = function() {
+  var todo = document.getElementById("inputField");
+  todo.value = "";
+  todo.focus();
+};
+
+function App(storageKey) {
+  var this_ = this;
+
+  this.createStorageInstance = function () {
+    var selectedOption = document.getElementById("storage").value;
+    var storage = {
+      key : storageKey,
+      LocalStorage: "LocalStorage",
+      SessionStorage: "SessionStorage"
+    };
+    return new StorageManager(storage[selectedOption], storage.key);
+  };
+  
+  this.storeData = function (taskData) {
+    this_.createStorageInstance().setData(taskData);
+  };
+  
+  this.getData = function () {
+      return this_.createStorageInstance().getData();
+  };
+
+  this.attachEvents = function() {
+    var addButton = document.getElementById("addBtn");
+    var todo = document.getElementById("inputField");
+    addButton.addEventListener("click", this_.addTaskOnClick);
+    todo.addEventListener("keypress", this_.addTaskOnKeyPress);
+  };
 
   this.selectStorageType = function() {
     var selectedOption = document.getElementById("storage");
-    selectedOption.addEventListener(
-      "change",
-      this_.onChoosingDataStorage
-    );
+    selectedOption.addEventListener("change",this_.onChoosingDataStorage);
   };
-
+  
   this.onChoosingDataStorage = function() {
     this_.displayTasks();
     this_.clearAndFocusTextField();
     this_.tasksCount();
   };
-  this.attachEvents = function() {
-    var addButton = document.getElementById("addBtn");
-    var todo = document.getElementById("inputField");
-    addButton.addEventListener("click", this_.addTaskToList);
-    todo.addEventListener("keypress", this_.addTaskToListByEnterKey);
-  };
 
+  this.displayTasks = function() {
+    var list = document.getElementById("todoList");
+    list.innerHTML = "";
+    var taskData = this_.getData();
+    for (i = 0; i < taskData.length; i++) {
+      viewInstance().createElements(taskData[i].id,taskData[i].name,taskData[i].status);
+    }
+    this_.attachEvents();
+  };
+  
   this.addTasks = function() {
     var notifyMessage = {
       inputField: "Enter Valid Input"
@@ -58,87 +93,45 @@ function App() {
     if (inputField === "") {
       alert(notifyMessage.inputField);
     } else {
-      var taskData = getData();
+      var taskData = this_.getData();
       todos = { id: Date.now(), name: inputField, status: false };
       taskData.push(todos);
-      storeData(taskData);
-      viewInstance().createElements(inputField);
+      this_.storeData(taskData);
+      viewInstance().createElements(todos.id,inputField,todos.status);
       this_.clearAndFocusTextField();
       this_.tasksCount();
     }
   };
 
-  this.displayTasks = function() {
-    var list = document.getElementById("todoList");
-    list.innerHTML = "";
-    var taskData = getData();
-    for (i = 0; i < taskData.length; i++) {
-      viewInstance().createElements(taskData[i].name);
-    }
-    this_.attachEvents();
-  };
-
-  this.addTaskToList = function() {
-    this_.addTasks();
-  };
-
-  this.addTaskToListByEnterKey = function() {
-    var enterKey = 13;
-    if (event.keyCode === enterKey) {
-      this_.addTasks();
-    }
-  };
-
   this.deleteTaskFromList = function() {
-    var deletingItem = this.previousElementSibling.textContent;
+    var itemId = this.previousSibling.id;
     this.parentNode.remove();
-    this_.removeTaskFromArray(deletingItem);
-    this_.clearAndFocusTextField();
+    this_.updateStorage(itemId);
     this_.tasksCount();
   };
 
-  this.removeTaskFromArray = function(deletingItem) {
-    var taskData = getData();
-    var deleteObject = taskData.find(function(element) {
-      return element.name === deletingItem;
+  this.updateStorage = function(itemId) {
+    var taskData = this_.getData();
+    var deleteObject = taskData.findIndex(function(element) {
+        return element.id === Number(itemId);
     });
-    var itemIndex = taskData.indexOf(deleteObject);
-    taskData.splice(itemIndex, 1);
-    storeData(taskData);
+    taskData.splice(deleteObject, 1);
+    this_.storeData(taskData);
   };
 
-  this.toggleStatus = function(e, item) {
-    var taskData = getData();
-    var checkStatus = e.target.checked;
-    item = this.nextSibling.textContent;
-    var selectedItem = taskData.find(function(element) {
-      return element.name === item;
+  this.toggleStatus = function(e) {
+    var taskData = this_.getData();
+    var checkboxStatus = e.target.checked;
+    var uniqueId = this.nextSibling.id;
+    var selectedItem = taskData.findIndex(function(element) {
+      return element.id === Number(uniqueId);
     });
-    var itemIndex = taskData.indexOf(selectedItem);
-    taskData[itemIndex].status = checkStatus;
-    storeData(taskData);
+    taskData[selectedItem].status = checkboxStatus;
+    this_.storeData(taskData);
     this_.tasksCount();
   };
 
-  this.tasksCount = function() {
-    var taskData = getData();
-    var checkbox = document.querySelectorAll('input[type="checkbox"]:checked');
-    var pending;
-    var checkedCount = 0;
-    for (var i = 0; i < checkbox.length; i++) {
-      checkboxCount = checkbox[i].checked ? checkedCount++ : checkedCount--;
-    }
-    pending = taskData.length - checkedCount;
-    viewInstance().totalMsg(checkedCount, pending);
-  };
-
-  this.clearAndFocusTextField = function() {
-    var todo = document.getElementById("inputField");
-    todo.value = "";
-    todo.focus();
-  };
-
-  this.attachCheckBoxEvent = function(selectionBox, span1) {
+  this.attachCheckBoxEvent = function(selectionBox) {
     selectionBox.addEventListener("click", this_.toggleStatus);
   };
 
@@ -146,4 +139,5 @@ function App() {
     deleteButton.addEventListener("click", this_.deleteTaskFromList);
   };
 }
+
 init();
--- src/view.js
+++ src/view.js
@@ -5,84 +5,75 @@ function viewInstance() {
 function View() {
   var _this = this;
 
-  this.createElements = function(input) {
+  this.createElements = function (id, input, status) {
     var li = document.createElement("li");
-    _this.tickbox(li);
-    _this.task(li, input);
+    _this.tickbox(li, status);
+    _this.task(li, input, id);
     _this.deleteButton(li);
     _this.appendToList(li);
   };
 
-  this.appendToList = function(createdElements) {
+  this.appendToList = function (createdElements) {
     var list = document.getElementById("todoList");
     list.appendChild(createdElements);
   };
 
-  this.tickbox = function(li) {
-    var selectionBox = document.createElement("input");
-    selectionBox.type = "checkbox";
-    selectionBox.setAttribute("id", "check");
-    controllerInstance().attachCheckBoxEvent(selectionBox);
-    li.appendChild(selectionBox);
-  };
+  View.prototype = new ListItems();
 
-  this.task = function(li, inputField) {
-    var spanElement = document.createElement("span");
-    var item = document.createTextNode(inputField);
-    spanElement.appendChild(item);
-    li.appendChild(spanElement);
-  };
+  function ListItems() {
 
-  this.deleteButton = function(li) {
-    var deleteButton = document.createElement("button");
-    deleteButton.innerHTML = "Del";
-    deleteButton.setAttribute("class", "remove");
-    controllerInstance().attachDeleteButtonEvent(deleteButton);
-    li.appendChild(deleteButton);
-  };
+    this.tickbox = function (li, status) {
+      var selectionBox = document.createElement("input");
+      selectionBox.type = "checkbox";
+      selectionBox.setAttribute("id", "tick");
+      selectionBox.checked = status;
+      controllerInstance.attachCheckBoxEvent(selectionBox);
+      li.appendChild(selectionBox);
+    };
 
-  this.totalMsg = function(checkedCount, pending) {
+    this.task = function (li, inputField, id) {
+      var spanElement = document.createElement("span");
+      spanElement.id = id;
+      var item = document.createTextNode(inputField);
+      spanElement.appendChild(item);
+      li.appendChild(spanElement);
+    };
+
+    this.deleteButton = function (li) {
+      var deleteButton = document.createElement("button");
+      deleteButton.innerHTML = "Del";
+      deleteButton.setAttribute("id", "remove");
+      controllerInstance.attachDeleteButtonEvent(deleteButton);
+      li.appendChild(deleteButton);
+    };
+  }
+  
+  this.totalMsg = function (checkedCount, pending) {
     var totalTodos = document.getElementById("allTask");
     var countMessage = {
       all: "All Tasks: ",
       completed: " / Number of Completed Tasks: ",
       pending: " / Number of Pending Tasks: "
     };
-    var taskData = getData();
-    return (totalTodos.innerHTML =
-      countMessage.all +
-      taskData.length +
-      " " +
-      countMessage.completed +
-      checkedCount +
-      " " +
-      countMessage.pending +
-      pending);
-  };
-
-  this.emptyListMsg = function() {
-    var totalTodos = document.getElementById("allTask");
-    var countMessage = {
-      all: "All Tasks: ",
-      completed: " / Number of Completed Tasks: ",
-      pending: " / Number of Pending Tasks: "
-    };
-    return (totalTodos.innerHTML =
-      countMessage.all +
-      "0" +
-      " " +
-      countMessage.completed +
-      "0" +
-      " " +
-      countMessage.pending +
-      "0");
-  };
-
-  this.emptyList = function() {
-    var empty =
-      "----------------------------List is Empty---------------------------";
-    var list = document.getElementById("todoList");
-    list.innerHTML = empty;
-    _this.emptyListMsg();
+    var taskData = controllerInstance.getData();
+    return (totalTodos.innerHTML = countMessage.all + taskData.length + " " + countMessage.completed + checkedCount + " " + countMessage.pending + pending);
   };
 }
+
+View.prototype.emptyListMsg = function () {
+  var totalTodos = document.getElementById("allTask");
+  var countMessage = {
+    all: "All Tasks: ",
+    completed: " / Number of Completed Tasks: ",
+    pending: " / Number of Pending Tasks: "
+  };
+  return (totalTodos.innerHTML = countMessage.all + "0" +" " + countMessage.completed + "0" + " " + countMessage.pending + "0");
+};
+
+View.prototype.emptyList = function () {
+  var empty =
+    "----------------------------List is Empty---------------------------";
+  var list = document.getElementById("todoList");
+  list.innerHTML = empty;
+  this.emptyListMsg();
+};
